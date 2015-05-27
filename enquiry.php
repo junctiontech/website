f32a70c8e6689fb62d2fae660588380561ec8f12
palakjunction <palakmeshram@ymail.com>
2015-05-27 10:47:36 +0530

    server backup
    server backup

--- enquiry.php
+++ enquiry.php
@@ -1,14 +1,38 @@
 <?php
-    $to = "info@junctiontech.in";
-    $subject = "Discuss Requirement";
-     $name= $_POST['name'] ;
-   $subject="Discuss Requirement";
-    $email = $_POST['email'] ;
-     
-    $headers = "From: $name";
-    $sent = mail($to, $subject, $email, $headers) ;
+$to = "info@junctiontech.in";
+$subject = "Discuss Requirement";
+$name= $_POST['name'] ;
+$email = $_POST['email'] ; 
+$requirement= $_POST['requirement'] ; 
+$message = "
+<html>
+<head>
+<title>Discuss Requirement</title>
+</head>
+<body>
 
-    if ($sent) { 
+<table>
+<tr>
+<th>Name</th>
+<th>email</th>
+<th>Requirement</th>
+</tr>
+<tr>
+<td>  $name </td>
+<td>  $email </td>
+<td>  $requirement</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+$sent =mail($to,$subject,$message,$headers);
+   if ($sent) { 
       header("Location: http://junctiontech.in/thankyou.html");
       exit();
     } else {
