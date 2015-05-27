10314fc17892b443f3be84caf852f4e18c15b3fa
palakjunction <palakmeshram@ymail.com>
2015-05-27 17:40:34 +0530

    contact us form
    contact us form

--- contact.php
+++ contact.php
@@ -5,9 +5,35 @@
    $subject="Contact Enquiry";
     $email = $_POST['email'] ;
     $message = $_POST['message'] ;
-    
+	
+  $mail = "
+<html>
+<head>
+<title>Discuss Requirement</title>
+</head>
+<body>
+
+<table>
+<tr>
+<th>Name</th>
+<th>email</th>
+<th>Requirement</th>
+</tr>
+<tr>
+<td>  $name </td>
+<td>  $email </td>
+<td>  $requirement</td>
+</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";  
     $headers = "From: $name";
-    $sent = mail($to, $subject, $message, $headers) ;
+    $sent = mail($to, $subject, $message,$mail, $headers) ;
 
     if ($sent) { 
       header("Location: http://junctiontech.in/thankyou.html");
