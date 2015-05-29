c3d1257307e3e01787244d850f104f7daf3bfe84
palakjunction <palakmeshram@ymail.com>
2015-05-29 13:10:50 +0530

    sitemap
    sitemap

---
+++ sitemap.php
@@ -0,0 +1,23 @@
+<?php 
+include 'menu.php'; 
+?>
+<div class="main-content" >
+	<br />
+		<div class="container" style="padding-left: 20px; padding-right: 20px; padding-top: 10px;">
+				<div class="row clearfix">
+					<div class="col-md-12 column sitemap-page">
+					<h3 class="text-primary">
+						<i class="fa fa-home"></i>
+							Software Development
+						</h3>
+							<div class="row clearfix">
+							<div class="col-md-4 column">
+							<ul class="sitemapChildUL"><li class="sitemapChildLI"><h3><a href="/web-development/" title="Web Technologies" style="color:#FF561C">Web Technologies</a></h3><ul><li><a href="/web-development/dot-net/" title="Microsoft .Net">Microsoft .Net</a></li><li><a href="/web-development/php/" title="PHP Frameworks">PHP Frameworks</a></li><li><a href="/web-development/java/" title="Java EE">Java EE</a></li><li><a href="/web-technologies/nodejs/" title="NodeJS">NodeJS</a></li></ul></li><li class="sitemapChildLI"><h3><a href="/mobile-apps-development/" title="Mobile Platforms" style="color:#FF561C">Mobile Platforms</a></h3><ul><li><a href="/mobile/android-apps-development/" title="Android">Android</a></li><li><a href="/mobile/iphone-app-development/" title=" iOS"> iOS</a></li><li><a href="/mobile-apps-development/windows-phone/" title="Windows Phone">Windows Phone</a></li></ul></li></ul>
+							</div>
+						</div>
+					</div>
+			</div>
+</div>
+<?php 
+include 'footer.php'; 
+?>
