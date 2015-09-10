0e0e4d0e636e20ef4839bb9f762aaf4970b3a13d
dev1 <dev1@junctiontech.in>
2015-09-10 15:44:51 +0530

    finish

--- enquiry.php
+++ enquiry.php
@@ -46,7 +46,7 @@ $mail->isSMTP();
 // 0 = off (for production use)
 // 1 = client messages
 // 2 = client and server messages
-$mail->SMTPDebug = 5;
+$mail->SMTPDebug = 0;
 
 //Ask for HTML-friendly debug output
 $mail->Debugoutput = 'html';
