29f44f368fbca51aad70db6efd855f24e20923b1
dev1 <dev1@junctiontech.in>
2015-09-10 15:55:24 +0530

    validation

--- job-apply.php
+++ job-apply.php
@@ -11,24 +11,24 @@ include 'menu.php';
 					<form class="contact-form validate" role="form" method="post" action="job_apply.php" enctype="multipart/form-data">
 						
 						<div class="form-group">
-							<input type="text" name="name" class="form-control" placeholder="Name:" name="min_field" data-validate="number,minlength[4]">
+							<input required type="text" name="name" class="form-control" placeholder="Name:" name="min_field" data-validate="number,minlength[4]">
 						</div>
 						<div class="form-group">
-							<input type="text" name="phone" class="form-control" placeholder="Mobile No:" name="min_field" data-validate="number,minlength[4]">
+							<input required type="text" name="phone" class="form-control" placeholder="Mobile No:" name="min_field" data-validate="number,minlength[4]">
 						</div>
 							<div class="form-group">
-							<input type="text" name="email" class="form-control" placeholder="E-mail:" name="email" data-validate="email">
+							<input type="email" required name="email" class="form-control" placeholder="E-mail:" name="email" data-validate="email">
 						</div>
 						
 							<div class="form-group">
-							<input type="text" name="employer" class="form-control" placeholder="Present Employer:" name="min_field" data-validate="number,minlength[4]">
+							<input type="text" required name="employer" class="form-control" placeholder="Present Employer:" name="min_field" data-validate="number,minlength[4]">
 						</div>
 						<div class="form-group">
-							<input type="text" name="designation" class="form-control" placeholder="Present Designation:" name="min_field" data-validate="number,minlength[4]">
+							<input type="text" required name="designation" class="form-control" placeholder="Present Designation:" name="min_field" data-validate="number,minlength[4]">
 						</div>
 						<div class="form-group">
 							<label class="control-label">Upload your resume</label>
-							<input type="file" name="filename" class="filestyle" >
+							<input required type="file" name="filename" class="filestyle" >
 							<span style="padding-right: 5px; font-weight: bold;">File Format : </span><span style="font-size: 13px;">Plain Text, Rich Text, HTML, Word Document or PDF</span>
 						</div>
 					
