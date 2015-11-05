4644aef5a81eb683af24b30a33737bf25053487c
dev1 <dev1@junctiontech.in>
2015-11-05 10:51:07 +0530

    emp trck

--- projects/emp_track/application/config/database.php
+++ projects/emp_track/application/config/database.php
@@ -64,8 +64,8 @@ $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
 
-echo '<pre>';
-print_r($db['default']);
-echo '</pre>';
+
+print_r($db['default']);die;
+
 /* End of file database.php */
 /* Location: ./application/config/database.php */
